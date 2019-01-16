61786d4f30bb994742f181312c32fd072468bbb9
Natdenice <Clad1987@hotmail.com>
2019-01-16 16:51:12 +0100

    classList selected added instead toggle

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ firstLi.addEventListener('mouseout', function() { // mouseout is an event which 
 
 for (var i = 0; i < lis.length; i++) {
   lis[i].addEventListener("mouseover", function() {
-    this.style.color = 'green';
+    this.classList.add('selected');
   });
   lis[i].addEventListener("mouseout", function() {
-    this.style.color = 'black';
+    this.classList.remove('selected');
   });
   lis[i].addEventListener("click", function() {
     this.classList.toggle('done');
